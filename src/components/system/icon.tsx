55eb260aa3c0177b1dd7112cb765e38203d1811b
Jahfer Husain <echo@jahfer.com>
2025-02-11 07:25:47 -0500

    Style tweaks to topic link in table

--- src/components/system/icon.tsx
+++ src/components/system/icon.tsx
@@ -6,7 +6,7 @@ interface IconProps {
   inline?: boolean,
 }
 
-export function Icon ({name, className, inline = false}: IconProps) {
+export function Icon ({name, className = '', inline = false}: IconProps) {
   return (
     <i className={`fa fa-${name} ${className} ${inline ? 'fa-inline' : ''}`}></i>
   )
